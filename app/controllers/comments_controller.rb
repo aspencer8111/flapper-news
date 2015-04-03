aea713fbdbd3e5a170df198943baccb0f378728c
Alex Spencer <alex@alexspencer.net>
2015-04-03 18:09:48 -0400

    initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+  before_filter :authenticate_user!, only: [:create, :upvote]
+  before_filter :get_post
+
+  def create
+    comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
+    respond_with @post, comment
+  end
+
+  def upvote
+    comment = @post.comments.find(params[:id])
+    comment.increment!(:upvotes)
+
+    respond_with @post, comment
+  end
+
+  private
+
+  def get_post
+    @post = Post.find(params[:post_id])
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:body, :upvotes, :user_id)
+  end
+end
